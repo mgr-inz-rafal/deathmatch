f23201d27d92768cc1a1cb7b8a5f4fafed51a3d6
mgr_inz_rafal <rchabowski@gmail.com>
2024-03-15 23:49:53 +0100

    Add connection limit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::sync::Arc;
+
 use clap::Parser;
 use futures::{StreamExt, TryStreamExt};
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, sync::Semaphore};
 use tokio_util::codec::Framed;
 use types::RequestCodec;
 
@@ -9,15 +11,34 @@ struct Args {
     /// Server bind address.
     #[arg(short, long)]
     bind_addr: String,
+    /// Maximum number of simultaneous connections.
+    #[arg(short, long)]
+    max_conn: u8,
 }
 
-async fn run_server(bind_addr: &str) {
+async fn run_server(bind_addr: &str, max_conn: u8) {
     let listener = TcpListener::bind(bind_addr).await.unwrap();
-    println!("Listening on: {}", bind_addr);
+    println!(
+        "Listening on {} with a limit of {max_conn} connections",
+        bind_addr
+    );
 
+    let semaphore = Arc::new(Semaphore::new(max_conn as usize));
     loop {
+        println!(
+            "Available permits: {}/{max_conn}",
+            semaphore.available_permits()
+        );
         let (socket, remote_addr) = listener.accept().await.unwrap();
         println!("Connection from {remote_addr} established");
+        let permit = match semaphore.clone().try_acquire_owned() {
+            Ok(permit) => permit,
+            Err(_) => {
+                println!("No more connection slots available, sorry");
+                drop(socket);
+                continue;
+            }
+        };
 
         let framed = Framed::new(socket, RequestCodec {});
         tokio::spawn(async move {
@@ -26,6 +47,7 @@ async fn run_server(bind_addr: &str) {
                 println!("got request from {remote_addr}: {}", request.unwrap());
             }
             println!("Connection to {remote_addr} closed");
+            drop(permit)
         });
     }
 }
@@ -35,6 +57,6 @@ async fn main() {
     let args = Args::parse();
 
     println!("Server running...");
-    run_server(&args.bind_addr).await;
+    run_server(&args.bind_addr, args.max_conn).await;
     println!("Server stopped");
 }
